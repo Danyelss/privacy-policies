4cf3d07ec6b4b3d659fef34fc1fa492e7ff4cd84
Daniel Mos <daniel.mos@techstackapps.com>
2024-09-09 19:46:01 +0300

    Privacy Policy commit #1

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <title>
+        Privacy Policy
+      </title>
+      <h1>Privacy Policy</h1>
+      <div className=''>
+        Last Updated: September 9 2024
+
+        <h3>1. Introduction</h3>
+        Welcome to <b>Contabb</b> ("the App"). We value your privacy and are committed to protecting your personal information. <br/>
+        This Privacy Policy outlines how we collect, use, and protect your information when you use our mobile application.
+
+        <h3>2. Information We Collect</h3>
+        The App does not collect, store, or transmit any personal data to external servers. <br/> 
+        All data generated or used by the App remains exclusively on your device and is not shared with us or any third parties.
+
+
+        <h3>3. Data Usage</h3>
+        The data you input or generate within the App is used solely for providing and improving the functionality of the App. <br/> 
+        Since all data remains on your device, we do not have access to or control over your information.
+
+        <h3>4. Third-Party Services</h3>
+        Our App does not integrate with any third-party services that require data transmission or storage outside your device.
+
+        <h3>5. Data Security</h3>
+        We implement reasonable and appropriate security measures to protect your data stored on your device. <br/>
+        However, the security of your device and the data within it is your responsibility. <br/> 
+        We recommend using strong passwords, enabling device encryption, and keeping your device software up to date to ensure your data remains secure.
+
+        <h3>6. No Age Restrictions</h3>
+        Our App is available to users of all ages. We do not impose any age restrictions for the use of the App. <br/> 
+        However, we emphasize that we do not collect, store, or process any personal data, regardless of the user's age.
+
+        <h3>7. Changes to This Privacy Policy</h3>
+        We may update this Privacy Policy from time to time. Any changes will be posted within the App and become effective upon posting. <br/>
+         We encourage you to review this Privacy Policy periodically for any updates.
+
+        <h3>8. Contact Us</h3>
+        If you have any questions or concerns about this Privacy Policy, please contact us at:
+
+        <p><b>devmdsoftware@gmail.com</b></p>
+      </div>
+    </div >
+  );
+}
+
+export default App;
